4d837a2d0385aeeed23ed04b3e42de04b563b51a
Ashlee <40704161+ashleethomas@users.noreply.github.com>
2018-08-08 17:18:46 -0400

    Final Project update

--- src/client/component/AwardCard.js
+++ src/client/component/AwardCard.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { Col, Container, Row, Button, Card, CardBody } from "reactstrap";
-
+const styles = {
+    minHeight: "60vh",
+    marginBottom: "10px"
+}
 
 const AwardCard = props => (
-    <Card>
+    <Card style={styles}>
         <CardBody className="mx-auto">
             <img alt="award" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgRtA7V2zw9zKSDstk9bx0PfU16-F_bXcSXRJ7eQCT1ZM3b-cG" width="50px" />
             <p>Badge Name</p>
@@ -17,7 +20,3 @@ const AwardCard = props => (
     </Card>
 )
 export default AwardCard;
-
-// https://www.iranketab.ir/Images/user.jpg
-
-// http://www.pngmart.com/files/3/Award-PNG-Photos.png
